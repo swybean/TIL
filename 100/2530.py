9263825753a0e31943d6003bb06b89d48879932a
swybean <swybean@gmail.com>
2024-01-26 17:35:51 +0900

    백준 풀이 1월 26일

--- 100/2530.py
+++ 100/2530.py
@@ -10,12 +10,11 @@ current_second = int(current_time[2])
 cooking_time = int(input())
 
 # 종료되는 시각 계산
-end_hour = (current_hour + (current_minute * 60 + current_second + cooking_time) // 60) % 24
-end_minute = (current_minute + cooking_time) % 60
-            
-
+end_hour = (current_hour + (((current_minute * 60 + current_second + cooking_time) // 60)) // 60) % 24
+end_minute = (current_minute + (cooking_time // 60)) % 60
+end_second = ((current_second + cooking_time) % 60) 
 # 결과 출력
-print(f'{end_hour} {end_minute}')
+print(f'{end_hour} {end_minute} {end_second}')
 
 
 
