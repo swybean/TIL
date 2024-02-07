cc04acb4f10acf0bc466db5f51f5cb6970f9ca6b
swybean <swybean@gmail.com>
2024-02-07 22:58:23 +0900

    2/7 Algorithm study in home

--- Algorithm_문제풀이/2024.02.07_문제풀이/2005. 파스칼 삼각형.py
+++ Algorithm_문제풀이/2024.02.07_문제풀이/2005. 파스칼 삼각형.py
@@ -75,18 +75,18 @@
 import sys
 sys.stdin = open('input4.txt', 'r')
 
-
-
-
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = list([0] * i for i in range(1, N+1))
+
     for i in range(N):
         arr[i][0], arr[i][-1] = 1, 1
+
     for i in range(1, N-1):
         for j in range(0, i):
             arr[i+1][j+1] = arr[i][j] + arr[i][j+1]
+            
     print(f'#{tc}')
     for i in arr:
         print(*i)
