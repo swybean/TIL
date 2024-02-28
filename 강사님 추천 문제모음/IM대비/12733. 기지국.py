495f9904b57571b31f60eb05ed72f83db456a149
swybean <swybean@gmail.com>
2024-02-28 23:10:41 +0900

    2/28 Algorithm Home

--- 강사님 추천 문제모음/IM대비/12733. 기지국.py
+++ 강사님 추천 문제모음/IM대비/12733. 기지국.py
@@ -5,8 +5,8 @@ A = 1, B = 2, C = 3 칸씩 커버가 가능한 기지국이라는 뜻
 N = 9
 인 경우로 연습
 '''
-
-
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
 
 T = int(input())
 for test_case in range(1, T+1):
@@ -15,4 +15,4 @@ for test_case in range(1, T+1):
     
     for i in range(N):
         for j in range(N):
-            if arr[i][j] == 
+            if arr[i][j] == 'A':
--- 강사님 추천 문제모음/추천 문제/1859. 백만장자 프로젝트.py
+++ 강사님 추천 문제모음/추천 문제/1859. 백만장자 프로젝트.py
@@ -1,29 +1,45 @@
 T = int(input())
-for test_case in range(1, T+1):
+for test_case in range(1, T + 1):
     N = int(input())
-    arr = list(map(int, input().split()))
+    price = list(map(int, input().split()))
+    maxprice = [0] * N
+    total = 0
+ 
+    # 마지막부터 역순으로 각 날짜 이후의 최대 가격 찾기
+    maxprice[N - 1] = price[N - 1]
+    for i in range(N - 2, -1, -1):
+        maxprice[i] = price[i] if price[i] > maxprice[i + 1] else maxprice[i + 1]
+        total += maxprice[i] - price[i]
+ 
+    print(f'#{test_case} {total}')
 
-    buy_price = 0       # 구매한 상품의 총 가격
-    num_buy = 0         # 구매해서 보유하고 있는 상품 개수
-    profit= 0           # 현재 이익
-    max_profit = 0      # 최대 이익
+#### 나중에 풀어보자.. 근데 아마 for문으로는 시간초과 뜬다고 하심 강사님이
+# T = int(input())
+# for test_case in range(1, T+1):
+#     N = int(input())
+#     arr = list(map(int, input().split()))
 
-    for i in range(N-1):
-        if arr[i] <= arr[i+1]:          # 현재 가격이 다음 가격보다 작으면 구매
-            buy_price += arr[i]         # 구매가에 현재 가격 추가
-            num_buy += 1                # 구매한 상품 수 1 증가
+#     buy_price = 0       # 구매한 상품의 총 가격
+#     num_buy = 0         # 구매해서 보유하고 있는 상품 개수
+#     profit= 0           # 현재 이익
+#     max_profit = 0      # 최대 이익
 
-        elif arr[i] > arr[i+1]:         # 현재 가격이 다음 가격보다 크면 판매
-                                        # 현재이익 = (구매수 x 현재 가격) - 구매가 총합
-            profit += (num_buy * arr[i]) - buy_price  
-            buy_price = 0               # 구매한 상품 가격 초기화
-            num_buy = 0                 # 구매해서 보유하고 있는 상품 개수 초기화
+#     for i in range(N-1):
+#         if arr[i] <= arr[i+1]:          # 현재 가격이 다음 가격보다 작으면 구매
+#             buy_price += arr[i]         # 구매가에 현재 가격 추가
+#             num_buy += 1                # 구매한 상품 수 1 증가
 
-    # 마지막 날에 상품을 모두 판매한 경우를 처리
-    profit += num_buy * arr[N-1]
-    max_profit += profit - buy_price
+#         elif arr[i] > arr[i+1]:         # 현재 가격이 다음 가격보다 크면 판매
+#                                         # 현재이익 = (구매수 x 현재 가격) - 구매가 총합
+#             profit += (num_buy * arr[i]) - buy_price  
+#             buy_price = 0               # 구매한 상품 가격 초기화
+#             num_buy = 0                 # 구매해서 보유하고 있는 상품 개수 초기화
+
+#     # 마지막 날에 상품을 모두 판매한 경우를 처리
+#     profit += num_buy * arr[N-1]
+#     max_profit += profit - buy_price
     
-    print(f'#{test_case} {max_profit}')
+#     print(f'#{test_case} {max_profit}')
 
 
 '''
