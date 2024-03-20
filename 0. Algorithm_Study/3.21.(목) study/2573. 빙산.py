9af521e287843a7589d5500597e072967deee892
swybean <swybean@gmail.com>
2024-03-21 07:31:22 +0900

    3/21 Algorithm study

--- 0. Algorithm_Study/3.21.(목) study/2573. 빙산.py
+++ 0. Algorithm_Study/3.21.(목) study/2573. 빙산.py
@@ -1,11 +1,34 @@
+from collections import deque
+
+def bfs(x, y):
+    queue = deque
+    queue.append(x, y)
+    visited[x][y] = 0
+
+    while queue:
+        i, j = queue.popleft()
+
+        for k in range(4):
+            ni = i + di[k]
+            nj = j + dj[k]
+            if 0 <= ni < N and 0 <= nj < M:
+                if arr[ni][nj] == 0:
+                    visited[i][j] += 1
+                
+                if arr[ni][nj] != 0 and visited[ni][nj] == 0:
+                    queue.append(ni, nj)
+                    visited[ni][nj] += 1
+
+
+# 4방향 델타 설정
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]  
 
 N, M = map(int, input().split())    # N은 행, M은 열
 
 arr = [list(map(int, input().split())) for _ in range(N)]
 
-
-
-
+visited = [[0] * M for _ in range(N)]
 
 
 
