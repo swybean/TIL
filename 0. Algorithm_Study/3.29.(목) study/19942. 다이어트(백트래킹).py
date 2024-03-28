44100340a7277b3477218318be6804916d3c5d7d
swybean <swybean@gmail.com>
2024-03-28 17:50:04 +0900

    3/28 Algorithm

--- 0. Algorithm_Study/3.29.(목) study/19942. 다이어트(백트래킹).py
+++ 0. Algorithm_Study/3.29.(목) study/19942. 다이어트(백트래킹).py
@@ -13,4 +13,36 @@ N = int(input())    # 식재료의 개수 N
 target = list(map(int, input().split())) # 최소 영양성분 리스트
 arr = [list(map(int, input().split())) for _ in range(N)]   # 식재료 성분 + 가격 리스트
 
+result = 5000    # 최소 식재료의 가격 합
+cost = 0
+
+# now = [0] * 5   # 여기에 현재 고른 식재료들의 영양성분 + 가격 합 저장하기
+# print(now)      # [0, 0, 0, 0, 0]
+
+print(target)   # [100, 70, 90, 10]
+
+print(arr)      
+# [
+# [30, 55, 10, 8, 100], 
+# [60, 10, 10, 2, 70], 
+# [10, 80, 50, 0, 50], 
+# [40, 30, 30, 8, 60], 
+# [60, 10, 70, 2, 120], 
+# [20, 70, 50, 4, 4]
+# ]
+
+for i in range(N):
+    if target[0] > 0 and target[1] > 0 and target[2] > 0 and target[3] > 0:
+        target[0] - arr[i][0]
+        target[1] - arr[i][1]
+        target[2] - arr[i][2]
+        target[3] - arr[i][3]
+        cost += arr[i][4]
+        if target[0] <= 0 and target[1] <= 0 and target[2] <= 0 and target[3] <= 0:
+            if result > cost:
+                result = cost
+                print(result)
+
+    
+
 
---
+++ 4. Django/DAY8.md
@@ -0,0 +1,26 @@
+# **Django_8일차** (3/28)
+ 
+## **목차**
+**Authentication System 1**
+
+**1. Cookie & Session**  
+-쿠키
+
+**2. Authentication System**  
+
+**3. Custom User model**  
+-User Model 대체하기
+
+**4. Login & Logout**  
+
+**5. Template with Authentication daga**  
+-템플릿과 인증 데이터
+
+---
+
+## **1. Cookie & Session**
+우리가 서버로 부터 받은 페이지를 볼 때, 우리는 서버와 서로 연결되어 있는 상태가 아니다.
+
+
+
+
