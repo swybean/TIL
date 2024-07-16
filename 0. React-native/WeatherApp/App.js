83a2e7d544b7680dce733ba8bf761056b35774c7
swybean <swybean@gmail.com>
2024-07-16 17:33:04 +0900

    React-native WeatherApp

--- 0. React-native/WeatherApp/App.js
+++ 0. React-native/WeatherApp/App.js
@@ -4,27 +4,101 @@ import { StatusBar } from "expo-status-bar";
 
 const App = () => {
   return (
-    <View style={{ flex: 1, flexDirection: "row" }}>
-      <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-      <View
-        style={{
-          flex: 4,
-          backgroundColor: "teal",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <Text
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+        <View
           style={{
-            fontSize: 50,
-            fontWeight: "bold",
-            color: "white",
+            flex: 4,
+            backgroundColor: "teal",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          연습중
+          <Text
+            style={{
+              fontSize: 40,
+              fontWeight: "bold",
+              color: "red",
+            }}
+          >
+            난 첫번째 뷰!
+          </Text>
+        </View>
+        <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+      </View>
+
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ color: "black", fontSize: 40, fontWeight: "bold" }}>
+          안녕 난 2번째 뷰야
         </Text>
       </View>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "black",
+        }}
+      >
+        <Text style={{ fontSize: 40, fontWeight: "bold", color: "white" }}>
+          그럼 난 3번째 뷰!
+        </Text>
+      </View>
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "red",
+        }}
+      >
+        <Text style={{ fontSize: 40, fontWeight: "bold", color: "yellow" }}>
+          그럼 난 4번째 뷰!
+        </Text>
+      </View>
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "blue",
+        }}
+      >
+        <Text style={{ fontSize: 40, fontWeight: "bold", color: "white" }}>
+          그럼 난 5번째 뷰!
+        </Text>
+      </View>
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "green",
+        }}
+      >
+        <Text style={{ fontSize: 40, fontWeight: "bold", color: "black" }}>
+          그럼 난 6번째 뷰!
+        </Text>
+      </View>
+
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "yellow",
+        }}
+      >
+        <Text style={{ fontSize: 40, fontWeight: "bold", color: "blue" }}>
+          럭키세븐!!!!!!!
+        </Text>
+      </View>
+
       <StatusBar></StatusBar>
     </View>
   );
